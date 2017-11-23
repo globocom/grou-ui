3c7c69da869ed08af2f23e53dfa2468cde0326e2
thaissa.falbo <thaissa.falbo@gmail.com>
2017-11-23 10:53:04 -0200

    changes form field

--- src/components/RequestTests.js
+++ src/components/RequestTests.js
@@ -11,7 +11,7 @@ export default class RequestTests extends Component {
      this.sendForm = this.sendForm.bind(this);
      this.setName = this.setName.bind(this);
      this.setProject = this.setProject.bind(this);
-     this.setProperties = this.setProperties.bind(this);
+     this.setDurationTime = this.setDurationTime.bind(this);
    }
 
 
@@ -25,13 +25,14 @@ export default class RequestTests extends Component {
    }
 
     sendForm(event){
+      console.log(JSON.stringify({name:this.state.name, project:this.state.project, durationTimeMillis:this.state.durationtime}));
      event.preventDefault();
      $.ajax({
       url:'http://localhost:8080/tests?x-auth-token=gAAAAABaFDGJ0JKcKFZGI1dxHHWdSKhTLUcMMu7E7rLV9QlcgwWkP2Cu2YZjZbLmxlvJtLnguFkipsniocnrFdujyGGXWc3gCcVuXYZKeewlqK7DX2IUKL_T1RGZGUnqosETzPh7yxW5vHkSjxp8N3bltIPerItrVr11MGvySj1ZrKAgBugip1Y',
       contentType:'application/json',
       dataType:'json',
       type:'post',
-      data: JSON.stringify({name:this.state.name, project:this.state.project, properties:this.state.properties}),
+      data: JSON.stringify({name:this.state.name, project:this.state.project, durationTimeMillis:this.state.durationtime}),
       error: function(resposta){
       console.log("erro");
      }
@@ -46,8 +47,8 @@ export default class RequestTests extends Component {
       this.setState({project:event.target.value});
     }
 
-    setProperties(event){
-      this.setState({properties:event.target.value});
+    setDurationTime(event){
+      this.setState({durationtime:event.target.value});
     }
 
     render(){
@@ -63,7 +64,7 @@ export default class RequestTests extends Component {
               <form onSubmit={this.sendForm} method="post">
                 <CustomInput id="name" type="text" name="name" value={this.state.name} onChange={this.setName} label="Name"/>
                 <CustomInput id="project" type="text" name="project" value={this.state.project} onChange={this.setProject} label="Project"/>
-                <CustomInput id="properties" type="text" name="properties" value={this.state.properties} onChange={this.setProperties} label="Properties"/>
+                <CustomInput id="durationtime" type="text" name="durationtime" value={this.state.durationtime} onChange={this.setDurationTime} label="DurationTime"/>
                 <button type="submit" className="button is-primary">Submit</button>
               </form>
             </section>
